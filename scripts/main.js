655b33c08170fcbc8b6a7480b4a003a5a6d59805
Calafeteanu Marius <calafmarius2021@gmail.com>
2021-04-09 00:23:24 +0300

    Fixed a dropdown problem.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,28 @@
+//dropdown
+const showCategoryBtn = document.querySelector('.show-category');
+let listDropped = false;
+
+showCategoryBtn.addEventListener('click', () => {
+    if (listDropped === false) {
+        dropDownContainer.style.opacity = 1;
+        dropDownContainer.style.pointerEvents = 'all';
+        listDropped = true;
+    }
+    else {
+        dropDownContainer.style.opacity = 0;
+        dropDownContainer.style.pointerEvents = 'none';
+        listDropped = false;
+    }
+})
+
+
 // showing recipe categories in the dropdown list
 const dropDownContainer = document.querySelector('.dropdown');
 
 const getCategories = async () => {
     const categoriesFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
     const categoriesConvertJson = await categoriesFetch.json();
-    const categories = categoriesConvertJson.categories;
+    const categories = await categoriesConvertJson.categories;
 
     categories.forEach(category => {
         dropDownContainer.innerHTML += 
@@ -13,3 +31,10 @@ const getCategories = async () => {
 }
 
 window.addEventListener('load', getCategories);
+
+
+//adding selected category to results
+dropDownContainer.addEventListener('click', (e) => {
+    console.log(e.target.innerHTML)
+})
+
