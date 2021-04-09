1b09f70889ed7412930110c012a1af4d35c3e15b
Calafeteanu Marius <calafmarius2021@gmail.com>
2021-04-09 14:29:20 +0300

    Finished recipes by category + implemented SASS.

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,6 @@ showCategoryBtn.addEventListener('click', () => {
     }
 })
 
-
 // showing recipe categories in the dropdown list
 const dropDownContainer = document.querySelector('.dropdown');
 
@@ -34,7 +33,50 @@ window.addEventListener('load', getCategories);
 
 
 //adding selected category to results
-dropDownContainer.addEventListener('click', (e) => {
-    console.log(e.target.innerHTML)
-})
+const addCategoryToResults = async e => {
+    
+    const recipeOutput = document.querySelector('.recipe-output');
+    const resultsContainer = document.querySelector('.results');
+    const noResultMessage = document.querySelector('.no-results');
 
+    //display none to no-result message
+    noResultMessage.style.display = 'none';
+
+    //removing flex display from results container
+    resultsContainer.style.display = 'block';
+    resultsContainer.style.justifyContent = 'unset';
+    resultsContainer.style.alignItems = 'unset';
+
+    //setting the recipe output to grid
+    recipeOutput.style.display = 'grid';
+
+
+    //clearing previous filtering
+    recipeOutput.innerHTML = '';
+
+    const selectedRecipeCategory = e.target.innerHTML;
+
+    //fetching recipes based on the category selected
+    const fetchFilteredCategories = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedRecipeCategory}`);
+    const convertFilteredCategories = await fetchFilteredCategories.json();
+    const filteredCategories = await convertFilteredCategories.meals;
+    
+    // adding recipes on results
+    filteredCategories.forEach(recipe => {
+       setTimeout(() => {
+        recipeOutput.innerHTML += 
+        `<div class="recipe">
+                <div class="recipe-image">
+                    <img height="100%" width="100%" src="${recipe.strMealThumb}"></img>
+                </div>
+                <div class="recipe-description">
+                    <p class="recipe-category">${selectedRecipeCategory}</p>
+                    <p class="recipe-name">${recipe.strMeal}</p>
+                </div>
+        </div>`;
+       }, 500);
+    })
+
+}
+
+dropDownContainer.addEventListener('click', addCategoryToResults);
