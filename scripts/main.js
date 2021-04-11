1c0e1bb0acf0d9e5a61716a06e5932c5ffd553d3
Calafeteanu Marius <calafmarius2021@gmail.com>
2021-04-11 14:23:12 +0300

    Added search by keyword feature.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,24 @@
-//dropdown
+//var
+const dropDownContainer = document.querySelector('.dropdown');
+const recipeOutput = document.querySelector('.recipe-output');
+const searchBtn = document.querySelector('.search-btn');
+const noResultMessage = document.querySelector('.no-results');
+const resultsContainer = document.querySelector('.results');
+
+// adding recipe categories on window load
+const getCategories = async () => {
+    const categoriesFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+    const categoriesConvertJson = await categoriesFetch.json();
+    const categories = await categoriesConvertJson.categories;
+
+    categories.forEach(category => {
+        dropDownContainer.innerHTML += 
+        `<li class="category">${category.strCategory}</li>`;
+    })
+}
+window.addEventListener('load', getCategories);
+
+//dropdown script
 const showCategoryBtn = document.querySelector('.show-category');
 let listDropped = false;
 
@@ -15,48 +35,27 @@ showCategoryBtn.addEventListener('click', () => {
     }
 })
 
-// showing recipe categories in the dropdown list
-const dropDownContainer = document.querySelector('.dropdown');
 
-const getCategories = async () => {
-    const categoriesFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
-    const categoriesConvertJson = await categoriesFetch.json();
-    const categories = await categoriesConvertJson.categories;
 
-    categories.forEach(category => {
-        dropDownContainer.innerHTML += 
-        `<li class="category">${category.strCategory}</li>`;
-    })
+// deleting "no result" style
+const deleteStyle = () => {
+    noResultMessage.style.display = 'none';
+    resultsContainer.style.display = 'block';
+    resultsContainer.style.justifyContent = 'unset';
+    resultsContainer.style.alignItems = 'unset';
+    recipeOutput.style.display = 'grid';
+    recipeOutput.innerHTML = '';
 }
 
-window.addEventListener('load', getCategories);
 
 
 //adding selected category to results
 const addCategoryToResults = async e => {
-    
-    const recipeOutput = document.querySelector('.recipe-output');
-    const resultsContainer = document.querySelector('.results');
-    const noResultMessage = document.querySelector('.no-results');
 
-    //display none to no-result message
-    noResultMessage.style.display = 'none';
-
-    //removing flex display from results container
-    resultsContainer.style.display = 'block';
-    resultsContainer.style.justifyContent = 'unset';
-    resultsContainer.style.alignItems = 'unset';
-
-    //setting the recipe output to grid
-    recipeOutput.style.display = 'grid';
-
-
-    //clearing previous filtering
-    recipeOutput.innerHTML = '';
-
+    deleteStyle();
     const selectedRecipeCategory = e.target.innerHTML;
 
-    //fetching recipes based on the category selected
+    //fetching data
     const fetchFilteredCategories = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedRecipeCategory}`);
     const convertFilteredCategories = await fetchFilteredCategories.json();
     const filteredCategories = await convertFilteredCategories.meals;
@@ -78,5 +77,37 @@ const addCategoryToResults = async e => {
     })
 
 }
-
 dropDownContainer.addEventListener('click', addCategoryToResults);
+
+
+
+
+//showing recipes on search
+const searchRecipe = async () => {
+    const searchInput = document.querySelector('.search-input');
+    const keyWord = searchInput.value;
+
+    deleteStyle();
+
+    //fetching data
+    const recipeFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyWord}`);
+    const recipeConverted = await recipeFetch.json();
+    const recipes = await recipeConverted.meals;
+
+    // adding recipes on results
+    recipes.forEach(recipe => {
+        setTimeout(() => {
+            recipeOutput.innerHTML += 
+        `<div class="recipe">
+                <div class="recipe-image">
+                    <img height="100%" width="100%" src="${recipe.strMealThumb}"></img>
+                </div>
+                <div class="recipe-description">
+                    <p class="recipe-category">${recipe.strCategory}</p>
+                    <p class="recipe-name">${recipe.strMeal}</p>
+                </div>
+        </div>`;
+        }, 500)
+    })
+}
+searchBtn.addEventListener('click', searchRecipe);
